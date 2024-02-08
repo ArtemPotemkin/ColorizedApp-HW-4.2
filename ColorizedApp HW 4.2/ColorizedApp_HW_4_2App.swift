f63a9ddb8951d7371efbfc77bba50e2ac93c0a91
Artem Potemkin <art.potemkin@gmail.com>
2024-02-08 23:25:59 +0300

    Initial Commit

---
+++ ColorizedApp HW 4.2/ColorizedApp_HW_4_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_HW_4_2App.swift
+//  ColorizedApp HW 4.2
+//
+//  Created by Артём Потёмкин on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_HW_4_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp HW 4.2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorizedApp HW 4.2
+//
+//  Created by Артём Потёмкин on 08.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
