f731558e5c909b09a4616b7e80ce1f6f5c1a2863
Artem Potemkin <art.potemkin@gmail.com>
2024-02-09 00:42:01 +0300

    first step - simple

--- ColorizedApp HW 4.2/ContentView.swift
+++ ColorizedApp HW 4.2/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Color(
+                red: redSliderValue/255,
+                green: greenSliderValue/255,
+                blue: blueSliderValue/255)
+            .frame(width: 250, height: 250)
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            Spacer()
+            
+            SliderView(value: $redSliderValue, tintColor: .red)
+            SliderView(value: $greenSliderValue, tintColor: .green)
+            SliderView(value: $blueSliderValue, tintColor: .blue)
+            Spacer()
         }
         .padding()
+    }
+        
+}
+
+struct SliderView: View {
+    @Binding var value: Double
+    let tintColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .frame(width: 35, height: 40)
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(tintColor)
+        }
     }
 }
 
